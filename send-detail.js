882b94a29f60306c4170b38c8a0aaf5dfa5773c2
rohit-madasu <rohitmadasu@gmail.com>
2024-05-11 17:05:21 +0530

    modified the json response

--- send-detail.js
+++ send-detail.js
@@ -8,7 +8,6 @@ function submitForm() {
     };
 
     fetch('http://192.168.29.248:80/app/notetaker/create', {
- //   fetch('http://192.168.29.248:3000/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -18,8 +17,9 @@ function submitForm() {
     })
     .then(response => {
         if (!response.ok) {
-            console.log('Response:', response.json());
-            throw new Error('Network response not ok');
+            return response.json().then(errorResponse => {
+                throw new Error('Network response not ok: ' + JSON.stringify(errorResponse));
+            });
         }
         return response.json();
     })

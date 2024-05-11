d393647d78a1cd48fa886b7109993c0bd90dff24
rohit-madasu <rohitmadasu@gmail.com>
2024-05-11 11:07:25 +0530

    debugged the send details and login screen

--- send-detail.js
+++ send-detail.js
@@ -1,29 +1,32 @@
 function submitForm() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
+
+    var data = {
+        username: username,
+        password: password
+    };
+
+    fetch('http://192.168.29.248:3000/api/notetaker/create', {
+ //   fetch('http://192.168.29.248:3000/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Origin': 'http://192.168.29.253'
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => {
+        if (!response.ok) {
+            console.log('Response:', response.json());
+            throw new Error('Network response not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Response:', data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 }
-
-var data = {
-    username : username,
-    password : password
-};
-
-fetch('http://localhost:3000', {
-    method : 'POST',
-    headers : {
-        'Content-Type': 'application/json',
-    },
-    body : JSON.stringify(data),
-})
-.then(response => {
-    if(!response.ok) {
-        throw new Error('Network response not ok');
-    }
-    return response.json();
-})
-.then(data => {
-    console.log('Response :', data);
-})
-.catch(error => {
-    console.error('Error' : error);
-});
